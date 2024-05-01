addf9178fb3c8d927f9159b3254d095dd6f66ef9
Rahul Bhardwaj <147973917+rahulxbhardwaj@users.noreply.github.com>
2024-05-01 04:48:27 +0000

    checking loading

--- components/courses.jsx
+++ components/courses.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import classNames from "classnames";
 import "../styles/courses.scss";
 import PaymentQr from "../public/paymentqr.png";
+
 const Courses = () => {
   const [subjects, setSubjects] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -29,9 +30,15 @@ const Courses = () => {
         setLoading(false); // Set loading to false on error as well
       });
   };
+
   useEffect(() => {
     getdata();
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // You can replace this with a loading spinner or any other placeholder content
+  }
+
   return (
     <>
       <div>
@@ -44,7 +51,7 @@ const Courses = () => {
                 "rounded-t-2xl": index === 0,
                 "rounded-b-2xl": index === array.length - 1,
                 "rounded-none": index !== 0 && index !== array.length - 1,
-              },
+              }
             )}
           >
             <Link to={`/notes/${id}`} passHref>
@@ -53,55 +60,6 @@ const Courses = () => {
           </div>
         ))}
       </div>
-
-
-      
-      
-      {/* <div class="parent-container">
-        <div class="container">
-          <div class="left-side">
-            <div class="card">
-              <div class="card-line"></div>
-              <div class="buttons"></div>
-            </div>
-            <div class="post">
-              <div class="post-line"></div>
-              <div class="screen">
-                <div class="dollar">₹</div>
-              </div>
-              <div class="numbers"></div>
-              <div class="numbers-line2"></div>
-            </div>
-          </div>
-
-          <div class="right-side">
-            <div class="new">Pay Here</div>
-
-            <svg
-              viewBox="0 0 451.846 451.847"
-              height="512"
-              width="512"
-              xmlns="http://www.w3.org/2000/svg"
-              class="arrow"
-            >
-              <path
-                fill="#cfcfcf"
-                data-old_color="#000000"
-                class="active-path"
-                data-original="#000000"
-                d="M345.441 248.292L151.154 442.573c-12.359 12.365-32.397 12.365-44.75 0-12.354-12.354-12.354-32.391 0-44.744L278.318 225.92 106.409 54.017c-12.354-12.359-12.354-32.394 0-44.748 12.354-12.359 32.391-12.359 44.75 0l194.287 194.284c6.177 6.18 9.262 14.271 9.262 22.366 0 8.099-3.091 16.196-9.267 22.373z"
-              ></path>
-            </svg>
-          </div>
-          <img src={PaymentQr} />
-        </div>
-        {/* <div class = "batch">
-       <h1>Batch 1</h1>
-      {/* </div> */}
-
-
-
-      
     </>
   );
 };
