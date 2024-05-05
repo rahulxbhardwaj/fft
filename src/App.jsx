8f1300c44ef39376162ea363e11f98948f48705e
Rahul Bhardwaj <147973917+rahulxbhardwaj@users.noreply.github.com>
2024-05-05 06:44:11 +0000

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Home from "../components/home.jsx";
 import Aboutus from "../components/aboutUs.jsx";
 import Courses from "../components/courses.jsx";
 import Notes from "../components/notes.jsx";
+import { Analytics } from "@vercel/analytics/react"
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <Router>
+      <Analytics/>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
